65421d695b9661bc77d47af94760cb20ca11bf1b
Juan Esteban Garcia <ctxuser@gmail.com>
2016-02-29 00:10:01 -0500

    Button to toggle sidenav

--- src/boot.js
+++ src/boot.js
@@ -1,8 +1,28 @@
 (function() {
+  'use strict';
+  
   angular.module('dataCollectorApp', ['ngMaterial'])
     .config(function($mdThemingProvider) {
       $mdThemingProvider.theme('default')
         .primaryPalette('deep-purple')
         .accentPalette('red');
     })
+    .controller('MainController', ['$mdSidenav', MainController]);
+
+  /**
+   * Main Controller for the Data Collector App
+   *
+   * @param $mdSidenav
+   * @constructor
+   */
+  function MainController($mdSidenav) {
+    var self = this;
+
+    self.toggleSidenav = toggleSidenav;
+
+    function toggleSidenav() {
+      $mdSidenav('left').toggle();
+    }
+
+  }
 })();
